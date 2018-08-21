33bf314f8f84963bbb1584bc0c96b5ef4e9af037
v_chenling <v_chenling@baidu.com>
2018-08-21 18:03:12 +0800

    add codes

---
+++ config/webpack.config.js
@@ -0,0 +1,57 @@
+/**
+ * development环境的webpack的配置文件
+ */
+const webpack = require('webpack'); // 访问内置插件
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    entry: path.join(__dirname, '../src/index.js'),
+    output: {
+        path: path.resolve(__dirname, 'output'),
+        filename: 'bundle.js'
+    },
+    devtool: 'eval-source-map',
+    devServer: {
+        port: 8009, // 端口号
+        hot: true, // 是否使用热更新
+        compress: true, // 压缩
+        historyApiFallback: true,
+        contentBase: path.join(__dirname, 'output') // 从哪里访问文件
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['es2015', 'react', 'stage-0'] // transform-class-properties这babel的四个插件缺一不可
+                        }
+                    },
+                exclude: /node_modules/
+            },
+            {
+                test: /\.(css|less)$/,
+                use: ['style-loader', 'css-loader', 'less-loader',
+                    {
+                        loader: 'less-loader',
+                        options: {javascriptEnabled: true} // 兼容less.3x以及3x以上的版本
+                    }
+                ],
+                exclude: /node_modules/
+            },
+            {
+                test: /\.(png|jpe?g|svg)$/,
+                use: 'url-loader'
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: path.join(__dirname, '../src/index.html')
+        }),
+        new webpack.HotModuleReplacementPlugin()
+    ]
+};
